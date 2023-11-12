02baf7583a6d0707ef9740329338e2c978addf7c
Nathan Mota <nathansmota@gmail.com>
2023-11-11 23:57:47 -0300

    :sparkles: feat: Estrutura inicial da página

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from 'react-icons/fi'
+import './style.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <h1>Buscador CEP</h1>
+
+      <div className="containerInput">
+
+        <input type="text" placeholder="Digite seu CEP"></input>
+
+        <button type="button">
+          <FiSearch size={25} color='#FFF' />
+          Buscar
+        </button>
+      </div>
+
+      <main className='name'>
+        <h2>CEP: 55555-222</h2>
+        <h2>Rua: Rua dos testes</h2>
+        <h2>Complemento: Complemento teste</h2>
+        <h2>Bairro: Bairro teste</h2>
+        <h2>Cidade: Cidade teste</h2>
+        <h2>Estado: Estado teste</h2>
+      </main>
+
     </div>
   );
 }
